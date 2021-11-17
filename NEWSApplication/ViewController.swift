3c44b00185f5d5975078254235926c78653b64f2
A00233642 <Taiwoakintunde26@gmail.com>
2021-11-17 12:32:29 -0500

    Initial Commit

---
+++ NEWSApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NEWSApplication
+//
+//  Created by PRO on 11/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
