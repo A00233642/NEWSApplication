00946ccd48521d28ebe2a91f5a1d15b4e182e69d
A00233642 <Taiwoakintunde26@gmail.com>
2021-11-28 08:16:10 -0500

    Create a new swift file within the project and name it APICaller with the fie create a class for APICaller, define a constant for the top headlinesURL. Create a function getTopStories, create an API Response.

---
+++ NEWSApplication/APICaller.swift
@@ -0,0 +1,68 @@
+//
+//  APICaller.swift
+//  NEWSApplication
+//
+//  Created by PRO on 11/17/21.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+       static let topHeadlinesURL = URL(string: "https://gnews.io/api/v4/top-headlines?token=e3fa86dafa51b478c55a53d7d9682052")
+    }
+    
+    private init() {}
+    
+    public func getTopStories(completion: @escaping (Result<[Article], Error>) ->Void) {
+        guard let url = Constants.topHeadlinesURL else  {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            else if let data = data {
+                do{
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
+                    
+                  
+                }
+                
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+       
+        task.resume()
+    }
+    
+}
+
+//create models
+
+struct APIResponse: Codable{
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+    
+}
+
+struct Source: Codable{
+    let name: String
+}
