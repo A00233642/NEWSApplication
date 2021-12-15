86fcb153dceabbbd9d3e43746cb8dafef9bd074c
A00233642 <Taiwoakintunde26@gmail.com>
2021-12-15 04:44:14 -0500

    Within the view controller createSearchBar then create a function for it, create an instance searchVC, Add a searchBarDelegate, Add a single function for searchBar, then get the text of the searchBar

--- NEWSApplication/ViewController.swift
+++ NEWSApplication/ViewController.swift
@@ -7,23 +7,26 @@
 
 import UIKit
 
+import SafariServices
 
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     
     private let tableView: UITableView = {
         let table = UITableView()
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
         return table
     }()
     
+    private let searchVC = UISearchController(searchResultsController: nil)
+    
+   private var viewModels = [NewsTableViewCellViewModel]()
+    private var articles = [Article]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        
         title = "News"
         view.addSubview(tableView)
         view.addSubview(tableView)
@@ -31,8 +34,41 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         view.backgroundColor = .systemBackground
         
-      
-        APICaller.shared.getTopStories{result in
+        fetchTopStories()
+        createSearchBar()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func createSearchBar() {
+        navigationItem.searchController = searchVC
+        searchVC.searchBar.delegate = self
+        
+    }
+    
+    private func fetchTopStories(){
+        APICaller.shared.getTopStories{ [weak self] result in
+            switch result{
+            case .success(let articles):
+                self?.articles = articles
+                self?.viewModels = articles.compactMap({
+                    NewsTableViewCellViewModel(
+                    title: $0.title,
+                    subtitle: $0.description ?? "No Description",
+                    imageURL: URL(string: $0.urlToImage ?? ""))
+                })
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
             
         }
     
@@ -41,29 +77,51 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
     
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        tableView.frame = view.bounds
-    }
+  
+    //Table
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return viewModels.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: "cell",
+       guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: NewsTableViewCell.identifier,
             for: indexPath
-        )
-        cell.textLabel?.text = "something"
+        ) as? NewsTableViewCell else{
+            fatalError()
+        }
+        cell.configure(with: viewModels[indexPath.row])
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        let article = articles[indexPath.row]
+        
+        guard let url = URL(string: article.url ?? "")
+        else
+        {
+            return
+        }
+        
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
     }
 
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    //search
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text, !text.isEmpty else{
+            return
+        }
+        
+        print(text)
+    }
 }
 
 
