00c6b1b2df90e8166a97db13d6363a6264379a37
A00233642 <Taiwoakintunde26@gmail.com>
2021-12-15 04:20:44 -0500

    searchers is called a string, create url string for the search bar

--- NEWSApplication/APICaller.swift
+++ NEWSApplication/APICaller.swift
@@ -11,7 +11,9 @@ final class APICaller {
     static let shared = APICaller()
     
     struct Constants {
-       static let topHeadlinesURL = URL(string: "https://gnews.io/api/v4/top-headlines?token=e3fa86dafa51b478c55a53d7d9682052")
+       static let topHeadlinesURL = URL(string: "https://gnews.io/api/v4/top-headlines?token=e3fa86dafa51b478c55a53d7d9682052&lang=en")
+       // https://gnews.io/api/v4/top-headlines?token=e3fa86dafa51b478c55a53d7d9682052&lang=en
+        static let searchUrlstring = "https://gnews.io/api/v4/search?q=example&token=e3fa86dafa51b478c55a53d7d9682052&lang=en"
     }
     
     private init() {}
@@ -45,7 +47,41 @@ final class APICaller {
         task.resume()
     }
     
+    public func search(with query: String, completion: @escaping (Result<[Article], Error>) ->Void) {
+        guard !query.trimmingCharacters(in: .whitespaces).isEmpty else{
+            return
+        }
+        let urlstring = Constants.searchUrlstring + query
+        guard let url = URL(string: urlstring) else  {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            else if let data = data {
+                do{
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
+                    
+                  
+                }
+                
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+       
+        task.resume()
+    }
+    
 }
+
 
 //create models
 
